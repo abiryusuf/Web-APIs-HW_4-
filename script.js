83bac121bd203039bc06dc12e34b341f77c4575d
abir yusuf <yusuf_ctg@yahoo.com>
2020-03-10 13:12:52 -0400

    update

--- script.js
+++ script.js
@@ -1,106 +1,269 @@
-var op1 = document.querySelector(".option1");
-var op2 = document.querySelector(".option2");
-var op3 = document.querySelector(".option3");
-var op4 = document.querySelector(".option4");
-var question =document.querySelector(".question");
-var questionNum = document.querySelector(".question-num-value");
-var totalQuestion =document.querySelector(".total-question");
-var options = document.querySelector(".options").children;
-var ansTracker = document.querySelector(".answers-tracker");
-var qusIndex;
-var timeStart = document.querySelector("#start");
+var beginButton = document.querySelector("#begin");
+var timer = document.querySelector("#timer");
+//Count in second
+var time = 60; 
+var quizContainer = document.querySelector('#quiz-box');
+var highscoreButton = document.querySelector('#view-highscores');
+var question = 0;
+var score = 0;
 
-var index =0;
-var secondsLeft =5;
+var highscoreInitials = [];
+var highscores = [];
 
-function setTime() {
-    var timerInterval = setInterval(function() {
-      secondsLeft--;
-      timeStart.textContent = secondsLeft + " seconds left to finish the coding quiz";
-  
-      if(secondsLeft === 0) {
-        clearInterval(timerInterval);
-        sendMessage();
-      }
-  
-    }, 1000);
-  }
-  setTime();
 var questions = [
-    {
-     q:" Which function of an Array object calls a function for each element in the array?",
-     options: ["a. ForEach","b. Every", "c. ForEvery", "d. Each"],
-     answer: 0
-    },
-    {
-        q:" How to write an ‘if’ statement for executing some code.If “i” is NOT equal to 8?",
-        options: ["a. Interface","b. Throws", "c. Program", "d. Short"],
-        answer: 0
-    },
-    {
-        q:"  What is the HTML tag under which one can write the JavaScript code?",
-        options: ["a. Javascript","b. Scripted", "c. Script", "d. JS"],
-        answer: 2
-    },
-    {
-        q:" In JavaScript, we do not have datatypes like integer and float. What is the function that can be used to check if the number is an integer or not?",
-        options: ["a. Integer","b. ifInteger","c. isInteger", "d. ifinteger"],
-        answer: 2
-    },
-    {
-        q:" Which of the following is an advantage of using JavaScript?",
-        options: ["a. Increased interacivity","b. Less server interaction", "c. Immediate feedback from the user", "d. All the above"],
-        answer: 3
-    }
-]
-totalQuestion.textContent =questions.length;
-function load(){
-    questionNum.textContent =index +1;
-   question.textContent = questions [qusIndex].q;
-   op1.textContent = questions [qusIndex].options[0];
-   op2.textContent = questions [qusIndex].options[1];
-   op3.textContent = questions [qusIndex].options[2];
-   op4.textContent = questions [qusIndex].options[3];
+	{
+		question: "Which function of an Array object calls a function for each element in the array?",
+		answers: [
+			"a. ForEach","b. Every", "c. ForEvery", "d. Each"
+		],
+		correctAnswer: 0
+	},
+	{
+		question: "How to write an ‘if’ statement for executing some code.If “i” is NOT equal to 8?",
+		answers: [
+			"a. Interface","b. Throws", "c. Program", "d. Short"
+		],
+		correctAnswer: 0
+	},
+	{
+		question: "What is the HTML tag under which one can write the JavaScript code?",
+		answers: [
+			"a. Javascript","b. Scripted", "c. Script", "d. JS"
+		],
+		correctAnswer: 2
+	},
+
+	{
+		question: "In JavaScript, we do not have datatypes like integer and float. What is the function that can be used to check if the number is an integer or not?",
+		answers: [
+			"a. Integer","b. ifInteger","c. isInteger", "d. ifinteger"
+		],
+		correctAnswer: 2
+	},
+	{
+		question: " Which of the following is an advantage of using JavaScript?",
+		answers: [
+			"a. Increased interacivity","b. Less server interaction", "c. Immediate feedback from the user", "d. All the above"
+		],
+		correctAnswer: 3
+	}
+];
+
+
+
+
+function showQuestion (questionNumber) {
+
+	var quizCard = document.createElement("div");
+	quizCard.className ="card";
+
+	quizContainer.appendChild(quizCard);
+
+
+
+	cardHeader = document.createElement("div");
+	cardHeader.className = "card-header";
+
+	cardHeader.textContent = questions[questionNumber].question;
+
+	quizCard.appendChild(cardHeader);
+
+	cardBody = document.createElement("div");
+	cardBody.className = "card-body";
+	quizCard.appendChild(cardBody);
+
+	questions[questionNumber].answers.forEach((answerOption, index) => {
+		var answerBtn = document.createElement("button");
+		answerBtn.className = "btn btn-primary answerBtn";
+		answerBtn.setAttribute("data-answer-index",index);
+		answerBtn.textContent = answerOption;
+		cardBody.appendChild(answerBtn);
+	});
+
+
+	var answerConfirm = document.createElement("div");
+	cardBody.appendChild(answerConfirm);
+
+	//build a function to increment the value 
+	cardBody.addEventListener('click', (event) => {
+		const isButton = event.target.nodeName === 'BUTTON';
+		if (isButton) {
+			answerSelected = parseInt(event.target.getAttribute("data-answer-index"));
+			if (answerSelected === questions[question].correctAnswer) {
+				score += 1;
+				answerConfirm.textContent="Correct!";
+				answerConfirm.setAttribute("style", "color: green; font-size: 20px")
+			}
+			else {
+				answerConfirm.textContent="Wrong!";
+				answerConfirm.setAttribute("style", "color: red; font-size: 20px")
+				time -= 5;
+			}
+			setTimeout(function(){
+				answerConfirm.textContent="";
+				quizContainer.removeChild(quizCard);
+				question+=1;
+				if (question < questions.length) {
+					showQuestion(question);
+				} 
+				},1000);
+
+		}
+	});
+
+};
+
+function answerConfirmation(confirm) {
+
+	setTimeout(function(){
+		confirmAnswer = quizContainer.textContent=msg;
+		},3000);
 }
-function check(element){
-  if(element.id == questions[qusIndex].answer){
-      element.classList.add("correct")
-      updateAnsTracker("correct");
-  }else{
-      element.classList.add("wrong");
-      updateAnsTracker("wrong");
+
+function quizComplete() {
+	if (time ===0){
+		quizContainer.removeChild(quizContainer.firstElementChild);
+	}
+	var quizCard = document.createElement("div");
+	quizCard.className ="card";
+
+	quizContainer.appendChild(quizCard);
+
+	cardHeader = document.createElement("div");
+	cardHeader.className = "card-header";
+
+	cardHeader.textContent = "Quiz Completed";
+
+	quizCard.appendChild(cardHeader);
+
+	cardBody = document.createElement("div");
+	cardBody.className = "card-body";
+	cardBody.textContent = "Your score was "+score;
+	
+	formElement = document.createElement("form");
+	formElement.className="form-inline";
+
+	inputTextCol = document.createElement("div");
+	inputTextCol.className="form-group mb-3";	
+	inputText = document.createElement("div");
+	inputText.textContent = "Enter Initials:";
+	inputTextCol.appendChild(inputText);
+
+	inputBoxCol = document.createElement("div");
+	inputBoxCol.className="form-group mb-3";
+	inputBox = document.createElement("input");
+	inputBox.type = "text";
+	inputBox.className = "form-control"
+	inputBox.id ="initial";
+	inputBoxCol.appendChild(inputBox);
+
+	submitButtonCol = document.createElement("div");
+	submitButtonCol.className = "form-group mb-3";
+	submitButton = document.createElement("button");
+	submitButton.className = "btn btn-primary answerBtn";
+	submitButton.textContent = "Submit";
+	submitButtonCol.appendChild(submitButton);
+
+	formElement.appendChild(inputTextCol);
+	formElement.appendChild(inputBoxCol);
+	formElement.appendChild(submitButtonCol);
+
+	cardBody.appendChild(formElement);
+	quizCard.appendChild(cardBody);
+
+	submitButton.addEventListener("click", function(event) {
+		event.preventDefault();
+	  
+		var nameInitial = document.querySelector("#initial").value;
+		highscoreInitials.push(nameInitial);
+		highscores.push(score);
+		localStorage.setItem("initial", JSON.stringify(highscoreInitials));
+		localStorage.setItem("score", JSON.stringify(highscores));
+
+		// defaults time
+		timer.textContent = 60;
+		score=0;
+		question=0;
+		quizContainer.removeChild(quizCard);
+		time=60;
+	  });
+
+}
+  
+
+function startTimer() {
+
+	if (quizContainer.childNodes.length >1) {
+		quizContainer.removeChild(quizContainer.firstElementChild);
+	}
+
+	if (time === 60){
+		showQuestion(question);
+		interval = setInterval(function() {
+			time--;
+			timer.textContent = time;
+			if(time===0 | question >= questions.length) {
+				clearInterval(interval);
+				
+				quizComplete();
+				return;
+			}
+
+		}, 1000);
+	}
   }
-  disableOptions()
-}
-function disableOptions(){
-    for(var i =0; i<options.length; i++){
-        options[i].classList.add("disabled");
-        if(options[i].id ==questions[qusIndex].answer){
-            options[i].classList.add("correct");
-        }
-    }
-}
 
-function randomQus(){
-    var randomNum = Math.floor(Math.random()*questions.length);
-    qusIndex = randomNum;
-    load();
+beginButton.addEventListener("click",startTimer);
 
-}
+highscoreButton.addEventListener("click",showHighscores);
 
-function answerTracker(){
-    for(var i =0; i<questions.length; i++){
-        var div = document.createElement("div");
-        div.textContent=
-        ansTracker.appendChild(div);
-    }
-}
-function updateAnsTracker(className){
-  ansTracker.children[index-1].classList.add(className);
-}
-window.onload = function(){
-    randomQus();
-    answerTracker();
-    
+function showHighscores() {
 
-}
+	if (quizContainer.childNodes.length >1) {
+		quizContainer.removeChild(quizContainer.firstElementChild);
+	}
+	var users = JSON.parse(localStorage.getItem("initial"));
+	var scores = JSON.parse(localStorage.getItem("score"));
+
+	var tbody =	document.createElement("table");
+	tbody.className ="table table-bordered";
+
+	var tr = document.createElement("tr");
+	var td1 = document.createElement("td");
+	td1.textContent = "Initial";
+	var td2 = document.createElement("td");
+	td2.textContent = "Score";
+	tr.appendChild(td1);
+	tr.appendChild(td2);
+	tbody.appendChild(tr);
+
+	for(i=0; i< users.length; i++) {
+		var tr = document.createElement("tr");
+
+		var td1 = document.createElement("td");
+		td1.textContent = users[i];
+		var td2 = document.createElement("td");
+		td2.textContent = scores[i];
+		tr.appendChild(td1);
+		tr.appendChild(td2);
+		tbody.appendChild(tr);
+	}
+
+	quizContainer.appendChild(tbody);
+
+	var clearButton = document.createElement("button");
+	clearButton.className = "btn btn-primary answerBtn";
+	clearButton.textContent = "Clear";
+	quizContainer.appendChild(clearButton);
+
+	clearButton.addEventListener("click",clearFunction);
+	
+	function clearFunction () {
+		localStorage.clear();
+		while (quizContainer.childNodes.length >1) {
+			quizContainer.removeChild(quizContainer.firstElementChild);
+		}
+
+	}
+
+ }
